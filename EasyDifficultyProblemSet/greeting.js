7bb39d6ef6929f59ae78f563ed3233c22f819abd
Parag Zaveri <parag.g.zaveri@gmail.com>
2017-12-29 17:16:08 -0600

    Solves greeting.js

---
+++ EasyDifficultyProblemSet/greeting.js
@@ -0,0 +1,16 @@
+
+const greeting = (name) => {
+	if (name) {
+		return `Hello, ${name}!`;
+	} else {
+		return 'Hello!';
+	}
+}
+
+
+
+// console.log(greeting());
+// console.log(greeting('Parag'));
+
+
+
