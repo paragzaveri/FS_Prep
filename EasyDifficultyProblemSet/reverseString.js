f92069b9c40dc30ac526e94f4d1039af51c9c155
Parag Zaveri <parag.g.zaveri@gmail.com>
2017-12-30 16:24:16 -0600

    Solves Reverse A String

---
+++ EasyDifficultyProblemSet/reverseString.js
@@ -0,0 +1,13 @@
+
+
+function reverseString(str){
+	var newStr = '';
+	for (var i = str.length - 1; i >= 0; i--){
+		newStr += str[i];
+	}
+	return newStr;
+}
+
+
+console.log(reverseString('hello'));
+
