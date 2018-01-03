fe55717d91b448a09cf1ab8908c7187f30d274e6
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-01-02 21:21:54 -0600

    Solves Flatten Array

---
+++ MediumDifficultyProblemSet/flattenArray.js
@@ -0,0 +1,15 @@
+function flatten(array){
+	var newArray = [];
+	for (var i = 0; i < array.length; i++){
+		if (Array.isArray(array[i])){
+			for (var j = 0; j < array[i].length; j++){
+				newArray.push(array[i][j]);
+			}
+		} else {
+			newArray.push(array[i]);
+		}
+	}
+	return newArray;
+}
+
+
